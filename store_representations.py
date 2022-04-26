29f8ce3a8264fd98a5604ff253737cdfce823691
lorenzobasile <lbasile@vipacco.phys.sissa.it>
2022-04-26 16:55:51 +0200

    fixes

--- store_representations.py
+++ store_representations.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 import torch
 from operations import monomial, composite, other
 from train import generate_data
+import os
 
 def main(args):
     torch.set_printoptions(threshold=10_000)
@@ -10,6 +11,8 @@ def main(args):
     with torch.no_grad():
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         for key, value in {**other, **monomial, **composite}.items():
+            if not os.path.exists(f'representations/{key}'):
+                os.makedirs(f'representations/{key}')
             print("operation: ", key)
             eq_token = args.p
             op_token = args.p + 1
@@ -18,7 +21,7 @@ def main(args):
                         dim=128, num_layers=2, num_heads=4, num_tokens=args.p + 2, seq_len=5
                     ).to(device)
             model.load_state_dict(torch.load(f'weights/{key}/final.pt'))
-            repr=model.extract_representation(data[:-1])
+            repr=model.extract_representation(data[:-1])[-1]
             print(repr.shape)
             torch.save(repr, f'representations/{key}/final.pt')
             print(torch.argmax(model(data[:-1])[-1], 1).reshape(97,96))
