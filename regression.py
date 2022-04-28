f007b87939433628e4c05bd36f1ab70c14ac7b36
lorenzobasile <lbasile@vipacco.phys.sissa.it>
2022-04-28 11:26:06 +0200

    regression

--- regression.py
+++ regression.py
@@ -1,6 +1,8 @@
 import torch
 from sklearn import linear_model
 from sklearn.metrics import r2_score
+from operations import monomial, other, composite
+import numpy as np
 
 torch.set_printoptions(threshold=10_000)
 
@@ -8,7 +10,12 @@ with torch.no_grad():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     representations={}
     for key, value in {**other, **monomial, **composite}.items():
-        representations[key]=torch.load(f'weights/{key}/final.pt')
+        representations[key]=torch.load(f'representations/{key}/final.pt')
 
-    lm = linear_model.Ridge(1e-8)
-    lm.fit(representations['x'], representations['x+y'])
+    lm = linear_model.LinearRegression()
+    x=representations['x'].cpu().numpy()
+    xy=representations['xy'].cpu().numpy()
+    x2=representations['x^2'].cpu().numpy()
+    print(x.shape)
+    lm.fit(x2, x)
+    print(r2_score(lm.predict(x2), x, multioutput='variance_weighted'))
--- store_representations.py
+++ store_representations.py
@@ -10,6 +10,7 @@ def main(args):
 
     with torch.no_grad():
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
         for key, value in {**other, **monomial, **composite}.items():
             if not os.path.exists(f'representations/{key}'):
                 os.makedirs(f'representations/{key}')
@@ -25,6 +26,20 @@ def main(args):
             print(repr.shape)
             torch.save(repr, f'representations/{key}/final.pt')
             print(torch.argmax(model(data[:-1])[-1], 1).reshape(97,96))
+
+        '''
+        eq_token = args.p
+        op_token = args.p + 1
+        data = generate_data(args.p, eq_token, op_token, monomial['x^2']).to(device)
+        model = Decoder(
+                    dim=128, num_layers=2, num_heads=4, num_tokens=args.p + 2, seq_len=5
+                ).to(device)
+        #model.load_state_dict(torch.load(f'weights/{key}/final.pt'))
+        repr=model.extract_representation(data[:-1])[-1]
+        print(repr.shape)
+        torch.save(repr, f'representations/x^2/random.pt')
+        print(torch.argmax(model(data[:-1])[-1], 1).reshape(97,96))
+        '''
 
 
 if __name__ == "__main__":
