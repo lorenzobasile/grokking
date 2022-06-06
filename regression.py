a9e00bd2892e64e065ea32a8be18b8274e99aad9
lorenzobasile <lbasile14@hotmail.it>
2022-06-06 15:12:45 +0200

    .

--- regression.py
+++ regression.py
@@ -16,10 +16,27 @@ with torch.no_grad():
         if key=='x^2':
             representationsNew[key]=torch.load(f'representations/{key}/random.pt')
 
-    x=representations['x'].cpu().numpy()
-    y=representations['x'].cpu().numpy()
-    result=get_cca_similarity(x.T, y.T)
-    print(result['cca_coef1'])
+    x=representations['y^2'].cpu().numpy().T
+    y=representationsNew['x^2'].cpu().numpy().T
+
+    x = x - np.mean(x, axis=1, keepdims=True)
+    y = y - np.mean(y, axis=1, keepdims=True)
+    
+    # Perform SVD
+    U1, s1, V1 = np.linalg.svd(x, full_matrices=False)
+    U2, s2, V2 = np.linalg.svd(y, full_matrices=False)
+    
+    svacts1 = np.dot(s1[:20]*np.eye(20), V1[:20])
+    # can also compute as svacts1 = np.dot(U1.T[:20], cacts1)
+    svacts2 = np.dot(s2[:20]*np.eye(20), V2[:20])
+    # can also compute as svacts1 = np.dot(U2.T[:20], cacts2)
+    
+    svcca_results = get_cca_similarity(svacts1, svacts2, epsilon=1e-10, verbose=False)
+
+    
+    #print(x.shape, y.shape)
+    #result=get_cca_similarity(x.T, y.T)
+    print(svcca_results['cca_coef1'])
     #lm = linear_model.LinearRegression()
     #x=representations['x+y'].cpu().numpy()
     #lm.fit(x_n, x)
