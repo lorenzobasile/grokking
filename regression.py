3afe49bcb53b30e1c83496c66a9ad949a5ca61e2
Lorenzo Basile <56440763+lorenzobasile@users.noreply.github.com>
2022-06-06 15:38:20 +0200

    svcca

--- regression.py
+++ regression.py
@@ -7,6 +7,19 @@ import numpy as np
 
 torch.set_printoptions(threshold=10_000)
 
+def svd_reduction(M):
+    M-=np.mean(M, axis=1, keepdims=True)
+    U,s,V=np.linalg.svd(M, full_matrices=False)
+    sv_sum = np.sum(s)
+    i = 0
+    quality_ratio = 0
+    while i < len(s) and quality_ratio < 0.99:
+        i += 1
+        quality_ratio = np.sum(s[:i])/sv_sum
+
+    M_reduced = np.dot(U[:, :i], s[:i]*np.eye(i))
+    return M_reduced
+
 with torch.no_grad():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     representations={}
@@ -16,24 +29,28 @@ with torch.no_grad():
         if key=='x^2':
             representationsNew[key]=torch.load(f'representations/{key}/random.pt')
 
-    x=representations['y^2'].cpu().numpy().T
-    y=representationsNew['x^2'].cpu().numpy().T
+    x=representations['y^2'].cpu().numpy()
+    y=representationsNew['x^2'].cpu().numpy()
 
+    x=svd_reduction(x).T
+    y=svd_reduction(y).T
+
+    '''
     x = x - np.mean(x, axis=1, keepdims=True)
     y = y - np.mean(y, axis=1, keepdims=True)
     
     # Perform SVD
     U1, s1, V1 = np.linalg.svd(x, full_matrices=False)
     U2, s2, V2 = np.linalg.svd(y, full_matrices=False)
-    
+
     svacts1 = np.dot(s1[:20]*np.eye(20), V1[:20])
     # can also compute as svacts1 = np.dot(U1.T[:20], cacts1)
     svacts2 = np.dot(s2[:20]*np.eye(20), V2[:20])
     # can also compute as svacts1 = np.dot(U2.T[:20], cacts2)
-    
-    svcca_results = get_cca_similarity(svacts1, svacts2, epsilon=1e-10, verbose=False)
+    '''
+    svcca_results = get_cca_similarity(x, y, epsilon=1e-10, verbose=False)
 
-    
+
     #print(x.shape, y.shape)
     #result=get_cca_similarity(x.T, y.T)
     print(svcca_results['cca_coef1'])
